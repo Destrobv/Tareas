58a7ad712c37ffb4392ba0308f60bee142e12550
Destrobv <Tirpitzwaifu1945@gmail.com>
2024-03-24 23:01:37 -0500

    Parámetros y retorno de valores en funciones

---
+++ Trabajo con diccionario.py
@@ -0,0 +1,24 @@
+#diccionario
+informacion_personal = {
+    "nombre": "josue",
+    "edad": 20,
+    "ciudad": "shell",
+    "profesion": "desconocida"
+}
+
+print(informacion_personal)
+
+#Modificar cuidad
+informacion_personal["ciudad"] = "quito"
+print(informacion_personal)
+
+#agregar profesion
+informacion_personal["profesion"] = "Ingeniero"
+print(informacion_personal)
+
+# Verificar si la clave "telefono" existe y agregarla si no existe
+if "telefono" not in informacion_personal:
+    informacion_personal["telefono"] = "123456789"
+
+informacion_personal.pop("edad")
+print(informacion_personal)
