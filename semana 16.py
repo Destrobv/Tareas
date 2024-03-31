fe3d29c914b0c623fc053ec35ac676d4b474a625
Destrobv <Tirpitzwaifu1945@gmail.com>
2024-03-31 17:10:15 -0500

    Parámetros y retorno de valores en funciones

--- semana 16.py
+++ semana 16.py
@@ -1,9 +1,9 @@
 my_notes = open("my_notes.txt", "w")
 
-my_notes.write("linea 1: hacer los pendientes . \n")
-my_notes.write("linea 2: ordenar las cosas .\n")
+my_notes.write("linea 1: hacer los pendientes. \n")
+my_notes.write("linea 2: ordenar la casa.\n")
 
-lineas = ["linea 3: hacer las compras .\n", "linea 4: hacer los trabajos pendientes.\n"]
+lineas = ["linea 3: hacer las compras.\n", "linea 4: hacer los trabajos pendientes.\n"]
 my_notes.writelines(lineas)
 
 my_notes.close()
