d601fed23c5a1d95aeb0865bc505e11b19721450
Destrobv <Tirpitzwaifu1945@gmail.com>
2024-03-31 19:31:49 -0500

    Parámetros y retorno de valores en funciones

--- semana 16.py
+++ semana 16.py
@@ -1,3 +1,4 @@
+# Abrir el archivo en modo escritura
 my_notes = open("my_notes.txt", "w")
 
 my_notes.write("linea 1: hacer los pendientes. \n")
@@ -6,15 +7,19 @@ my_notes.write("linea 2: ordenar la casa.\n")
 lineas = ["linea 3: hacer las compras.\n", "linea 4: hacer los trabajos pendientes.\n"]
 my_notes.writelines(lineas)
 
+# Cerrar el archivo después de la escritura
 my_notes.close()
 
+# Abrir el archivo nuevamente en modo lectura
 my_notes = open("my_notes.txt", "r")
 
+# Lectura del archivo usando el método read()
 print("método 1: read()")
 print("----------------------")
 print(my_notes.read())
 my_notes.close()
 
+# Lectura del archivo línea por línea usando el método readlines()
 my_notes = open("my_notes.txt", "r")
 print("método 2: readlines()")
 print("----------------------")
